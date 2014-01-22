70527a5ccd3586f4ef1d147562be877ae0fa3287
Alf Eaton <alf@hubmed.org>
2014-01-22 01:10:45 +0000

    table output with diffs

--- search.js
+++ search.js
@@ -1,9 +1,81 @@
 $(function() {
     var input = $('textarea');
     var form = $('form');
-    var output = $('ol');
+    var output = $('tbody');
 
     var template = Handlebars.compile($('#template').html());
+
+    $.ajaxQueue = function(params) {
+        return $.ajax(params);
+    };
+
+    var show = function(row, data, text, score) {
+        console.log(data);
+
+        data.text = text;
+        data.score = Math.round(score);
+
+        if (score > 6) {
+            data.confidence = 'high';
+        } else if (score > 3) {
+            data.confidence = 'medium';
+        } else {
+            data.confidence = 'low';
+        }
+
+        if (typeof data.author == 'string') {
+            data.author = [data.author];
+        }
+
+        $.each(data.author, function(index, author) {
+            data.author[index].given = author.given.split(/\s+/).map(function(name) {
+                return name.substr(0, 1);
+            }).join('');
+        });
+
+        data.etal = data.author.length > 10;
+        data.author = data.author.slice(0, 10);
+
+        data.year = data.issued['date-parts'][0][0];
+
+        row.append(template(data));
+        row.prettyTextDiff({ cleanup: true });
+    };
+
+    var fetch = function (row, url, text, score) {
+        var request = $.ajaxQueue({
+            url: url.replace(/dx.doi.org/, 'data.crossref.org'),
+            headers: { 'Accept': 'application/citeproc+json' }
+        }, { priority: true, tries: 1 });
+
+        request.done(function(data) {
+            show(row, data, text, score);
+        }).fail(function() {
+            show(row, {}, text);
+        });
+    };
+
+    var search = function(index, text) {
+        var row = $('<tr/>').appendTo(output);
+
+        var request = $.ajaxQueue({
+            url: 'http://search.crossref.org/dois',
+            data: { q: text },
+            dataType: 'json'
+        });
+
+        request.done(function(data) {
+            var result = data[0];
+
+            if (result) {
+                fetch(row, result.doi, text, result.score);
+            } else {
+                show(row, {}, text);
+            }
+        }).fail(function() {
+            show(row, {}, text);
+        });
+    };
 
     var update = function(event) {
         event.preventDefault();
@@ -12,76 +84,7 @@ $(function() {
 
         var items = input.val().split("\n");
 
-        var show = function(index, result) {
-            console.log(result);
-
-            var text = items[index];
-
-            if (!result.doi || !result.coins) {
-            //if (!result.match || !result.coins) {
-                $('<li/>', { text: text }).addClass('error').appendTo(output);
-                return;
-            }
-
-            var coins = $.deparam(result.coins.replace(/&amp;/g, '&'));
-
-            if (!coins.rft_id) {
-                $('<li/>', { text: text }).addClass('error').appendTo(output);
-                return;
-            }
-
-            var item = {
-                text: text,
-                score: result.score
-            };
-
-            $.each(coins, function(key, value) {
-                key = key.replace(/^rft\./, '');
-                item[key] = value;
-            });
-
-            if (typeof item.au == 'string') {
-                item.au = [item.au];
-            }
-
-            //item.url = item.rft_id.replace(/^info:doi\//, '');
-            item.url = result.doi;
-
-            output.append(template(item));
-        };
-
-        /*
-        var request = $.ajax({
-            type: 'POST',
-            url: 'http://search.crossref.org/links',
-            data: JSON.stringify(items),
-            contentType : 'application/json',
-            dataType: 'json'
-        });
-        */
-
-        $.each(items, function(index, item) {
-            var request = $.ajaxQueue({
-                url: 'http://search.crossref.org/dois',
-                data: { q: item },
-                dataType: 'json'
-            });
-
-            request.done(function(data) {
-                show(index, data[0]);
-            });
-        });
-
-        /*
-        var request = $.ajax({
-            url: 'result.json',
-            dataType: 'json'
-        });
-
-        request.done(function(data) {
-            $.each(data.results, fshow);
-        });
-        */
+        $.each(items, search);
     };
 
     form.on('submit', update);
